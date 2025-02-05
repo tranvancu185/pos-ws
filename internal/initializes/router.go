3937285e03c06c293ff79bbf48a5c680039d106f
tranvancu185 <harveytran185@gmail.com>
2025-02-05 22:07:01 +0700

    save

--- internal/initializes/router.go
+++ internal/initializes/router.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
 
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -72,8 +73,8 @@ func InitRouter() *gin.Engine {
 }
 
 func ExeStatic(router *gin.Engine) *gin.Engine {
-	// router.Use(static.Serve("/", static.LocalFile(global.Config.Path.PathWeb, true)))
+	router.Use(static.Serve("/", static.LocalFile(global.Config.Path.PathWeb, true)))
 	// router.Use(static.Serve("/", static.LocalFile("", true)))
-	router.Static("/home", "./public")
+	// router.Static("/home", "./public")
 	return router
 }
